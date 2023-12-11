dc00d18203ef6d8caf9f4a88a23be8b8dd625d7f
Fatma Ali <alifatma13@gmail.com>
2023-12-11 22:01:33 +0530

    UUID generated

--- src/main/java/com/productservice/models/BaseModel.java
+++ src/main/java/com/productservice/models/BaseModel.java
@@ -1,17 +1,20 @@
 package com.productservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.context.annotation.Primary;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public abstract class BaseModel {
     @Id
-    private Long id;
+    @GeneratedValue(generator = "generator_name")
+    @GenericGenerator(name = "generator_name", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
+    private UUID id;
 }

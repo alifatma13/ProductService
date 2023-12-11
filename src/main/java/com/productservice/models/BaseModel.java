afb0dd8d7423d5c3560114e3aafa081efde86919
Fatma Ali <alifatma13@gmail.com>
2023-12-11 19:12:14 +0530

    Database config added

--- src/main/java/com/productservice/models/BaseModel.java
+++ src/main/java/com/productservice/models/BaseModel.java
@@ -1,5 +1,17 @@
 package com.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.Primary;
+
+@Getter
+@Setter
+@MappedSuperclass
 public abstract class BaseModel {
+    @Id
     private Long id;
 }
